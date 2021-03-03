c7eea614ed54094646cb805002305246e808678e
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-03 06:17:49 +0530

    Attendence program is added

---
+++ program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace EmployeeAttendence1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is presesnt");
+            else
+                Console.WriteLine("Employee is absent");
+                Console.ReadKey();
+        }
+    }
+}
